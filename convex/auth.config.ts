e10ac12b19381c5ddaa39d70cc37bdf3da16e952
vctrubio <vctrubio@gmail.com>
2025-07-11 14:59:05 +0200

    jwt

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL,
+      domain: process.env.CONVEX_SITE_URL || "http://localhost:5173",
       applicationID: "convex",
     },
   ],
